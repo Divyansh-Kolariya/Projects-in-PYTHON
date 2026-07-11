46009599fe13d3f3e782ff1bc4d179aa7a1d6cb0
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-07-11 16:24:42 +0530

    fix score calculation and output formatting in Quiz.py

--- Quiz Game/Quiz.py
+++ Quiz Game/Quiz.py
@@ -6,27 +6,34 @@ if playing.lower() != "yes" and playing.lower() != "y":
     quit()
     
 print("Let's Play")
+Score = 0
 
 answer = input("What does CPU stands for? ")
 if answer.lower() == 'central processing unit':
     print("Correct")
+    Score += 1
 else:
     print("Incorrect")
 
 answer = input("Waht does GPU stands for? ")
 if answer.lower() == 'graphics processing unit':
     print("Correct")
+    Score += 1
 else:
     print("Incorrect")
     
 answer = input("What does RAM stands for? ")
 if answer.lower() == 'random access memory':
     print("Correct")
+    Score += 1
 else:
     print("Incorrect")
 
 answer = input("What does PSU stands for? ")
 if answer.lower() == 'power supply unit':
     print("Correct")
+    Score += 1
 else:
     print("Incorrect")
+
+print("You'r Score is",Score)
