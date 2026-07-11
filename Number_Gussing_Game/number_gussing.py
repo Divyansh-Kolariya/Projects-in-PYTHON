8d5afe706bcdf5205f66b88048c5a02574cc35a2
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-07-11 17:01:24 +0530

    refactor input handling and game loop in Number Guessing Game

--- Number_Gussing_Game/number_gussing.py
+++ Number_Gussing_Game/number_gussing.py
@@ -4,7 +4,7 @@ top_of_range = input("Enter a number: ")
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
-    
+
     if top_of_range < 0:
         print("Invald Number")
         quit()
@@ -13,6 +13,20 @@ else:
     quit()
 
 
-
 random_number = random.randrange(top_of_range)
 
+while True:
+    user_guess = input("Enter a Guess: ")
+
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Invald Number")
+        continue
+    
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    else:
+        print("Wrong Guess")
+    
