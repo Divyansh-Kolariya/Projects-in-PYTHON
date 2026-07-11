7df0879bdc930ffc550d23042f21456a7dd27983
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-07-11 16:09:50 +0530

    fix answer validation for quiz questions in Quiz.py

--- Quiz Game/Quiz.py
+++ Quiz Game/Quiz.py
@@ -12,3 +12,21 @@ if answer == 'central processing unit':
     print("Correct")
 else:
     print("Incorrect")
+
+answer = input("Waht does GPU stands for? ")
+if answer == 'graphics processing unit':
+    print("Correct")
+else:
+    print("Incorrect")
+    
+answer = input("What does RAM stands for? ")
+if answer == 'random access memory':
+    print("Correct")
+else:
+    print("Incorrect")
+
+answer = input("What does PSU stands for? ")
+if answer == 'power supply unit':
+    print("Correct")
+else:
+    print("Incorrect")
