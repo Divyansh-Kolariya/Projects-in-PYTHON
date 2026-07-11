b8ad6714ecaf7c934a158e004926aba85e652f0f
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-07-11 16:04:40 +0530

    add quiz question and answer validation to Quiz.py

--- Quiz Game/Quiz.py
+++ Quiz Game/Quiz.py
@@ -6,3 +6,9 @@ if playing != "yes":
     quit()
     
 print("Let's Play")
+
+answer = input("What does CPU stands for? ")
+if answer == 'central processing unit':
+    print("Correct")
+else:
+    print("Incorrect")
