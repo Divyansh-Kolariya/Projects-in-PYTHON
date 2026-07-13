3cd8f1bcd142e7ac9dad7655baf061681494d178
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-07-13 13:33:30 +0530

    refactor: streamline game logic and improve readability in Rock Paper Scissors implementation

--- Rock_Paper_Scissor/rock_paper_scissor.py
+++ Rock_Paper_Scissor/rock_paper_scissor.py
@@ -2,18 +2,33 @@ import random
 
 user_wins = 0
 computer_wins = 0
+options = ["rock", "paper", "scissors"]
 
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to Quit: ").lower()
     if user_input == 'q':
         break
         
-    if user_input not in ["rock", "paper", "scissors"]:
+    if user_input not in options:
         continue
     
     random_num = random.randint(0, 2)
     # rock: 0, paper: 1, scissors: 2
     
+    computer_pick = options[random_num]
+    print("Computer picked", computer_pick + ".")
     
+    if user_input == computer_pick:
+        print("Draw")
+    elif user_input[0] and computer_pick[2]:
+        user_wins += 1
+        print("You Won")
+    elif user_input[2] and computer_pick[1]:
+        user_wins += 1
+        print("You Won")
+    else:
+        print("You Lost, Total Score")
+        computer_wins += 1
+        
 
 print("Goodbye!")
