fe0e42006d2b1d47f6d0d06ff73c441ad4577f67
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-07-14 21:56:18 +0530

    fix: ensure user input is consistently converted to lowercase for valid options

--- Choose_Your_Own_Adveture/choose_your_own_adventure.py
+++ Choose_Your_Own_Adveture/choose_your_own_adventure.py
@@ -4,8 +4,20 @@ print("Welcome",name,"to this adventure!")
 answer = input("You are on a dirt road, it has come to an end you can go left or right. Which way would you like to go? ").lower()
 
 if answer == "left":
-    answer = input("You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ")
+    answer = input("You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ").lower()
+    if answer == "swim":
+        print("You swim across and were eaten by an alligator.")
+    elif answer == "walk":
+        print("You run out of water, you lost.")
+    else:
+        print("Not Valid Option")
 elif answer == "right":
-    print()
+    answer = input("You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back)? ").lower()
+    if answer == "back":
+        print("You go back and loss")
+    elif answer == "walk":
+        print("You Won!")
+    else:
+        print("Not Valid Option")
 else:
     print("Not Valid Option")
